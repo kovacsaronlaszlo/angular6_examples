1ff306e191d56bee78a2b933a42327a318e279b3
Kovács Áron László <k.aron.laszlo@gmail.com>
2018-07-07 16:38:58 +0200

    [create]: + create 5 component and pass them data + in app component determined an array and transfer data + create a product model where determined the variables and the types + how, when and why use @Input, @Output + what is EventEmitter, and HostBinding

---
+++ how-angular-works/inventory-app/src/app/product-row/product-row.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, HostBinding } from '@angular/core';
+import {Product} from "../product.model";
+
+@Component({
+  selector: 'app-product-row',
+  templateUrl: './product-row.component.html',
+  styleUrls: ['./product-row.component.css']
+})
+export class ProductRowComponent {
+  @Input() product: Product;
+  @HostBinding('attr.class') ccsClass = 'item';
+
+  constructor() { }
+
+
+}
