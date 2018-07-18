4f1756026639ea3ec010ef914cdb6245b9b12586
Kovács Áron László <k.aron.laszlo@gmail.com>
2018-07-18 22:05:12 +0200

    [create]: + in this section i got know the basic of Redux + made some tutorial example, and create a small Redux ts file +  I got know what is the different Redux and RxJs + and the end of this section create a small counter app with redux

---
+++ redux/redux-counter/src/app/counter.reducer.ts
@@ -0,0 +1,16 @@
+import {INCREMENT, DECREMENT} from './counter.actions';
+import {AppState} from "./app.state";
+import {Reducer, Action} from "redux";
+
+const initialState: AppState = {counter: 0};
+
+export const counterReducer: Reducer<AppState> = (state: AppState = initialState, action: Action): AppState => {
+  switch (action.type) {
+    case INCREMENT:
+      return Object.assign({}, state, { counter: state.counter + 1 });
+    case DECREMENT:
+      return Object.assign({}, state, { counter: state.counter - 1 });
+    default:
+      return state;
+  }
+};
---
+++ redux/tutorial/06-store-w-subscribe.ts
@@ -0,0 +1,72 @@
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+interface Reducer<T> {
+    (state: T, action: Action): T;
+}
+
+interface ListenerCallback {
+    (): void;
+}
+
+interface UnsubscribeCallback {
+    (): void;
+}
+
+class Store<T> {
+    private _state: T;
+    private _listeners: ListenerCallback[] = [];
+
+    constructor(
+        private reducer: Reducer<T>,
+        initialState: T
+    ) {
+        this._state = initialState;
+    }
+
+    getState(): T {
+        return this._state;
+    }
+
+    dispatch(action: Action): void {
+        this._state = this.reducer(this._state, action);
+        this._listeners.forEach((listener: ListenerCallback) => listener());
+    }
+
+    subscribe(listener: ListenerCallback): UnsubscribeCallback {
+        this._listeners.push(listener);
+        return () => {
+            this._listeners = this._listeners.filter(l => l !== listener);
+        };
+    }
+}
+
+let reducer: Reducer<number> = (state: number, action: Action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        case 'PLUS':
+            return state + action.payload;
+        default:
+            return state;
+    }
+};
+
+let store = new Store<number>(reducer,0);
+console.log(store.getState());
+
+let unsubscribe = store.subscribe(()=> {
+    console.log('subscribe: ', store.getState());
+});
+
+store.dispatch({type:'INCREMENT'});
+store.dispatch({type:'INCREMENT'});
+
+unsubscribe();
+store.dispatch({type: 'DECREMENT'});
+
+console.log(store.getState());
