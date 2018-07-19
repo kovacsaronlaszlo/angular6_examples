e19fa513d572b5303c85a8be1860286f6a9ad215
Kovács Áron László <k.aron.laszlo@gmail.com>
2018-07-19 21:56:04 +0200

    [create]: + create chat-app using redux + got bug at chat window component (if you know what is the problme please send mi a mail thx) + use what I got know the past section, and create a complex redux app

--- redux/redux-chat/src/app/app.module.ts
+++ redux/redux-chat/src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import {AppStore, appStoreProiders} from './app.store';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatPageComponent,
+    ChatNavBarComponent,
+    ChatThreadsComponent,
+    ChatThreadComponent,
+    ChatWindowComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [appStoreProiders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
